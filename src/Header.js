00efaf73b295e688e6931ccb01c7df5a505e2919
David Hevesi <davidhevesi2008@gmail.com>
2021-05-26 19:05:20 -0400

    Created navigation.

--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,35 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
-    <div className="header">
+    <div className="h-16 flex items-center bg-gray-800 sticky z-50">
       <img
-        className="header_logo"
+        className="w-24 object-contain mt-0 mb-5 m-5"
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
       />
-      <div className="header_search">
-        <input class="header_searchInput" type="text" />
+      <div className="flex flex-1 items-center border rounde-lg">
+        <input class="flex-1" type="text" />
+        <SearchIcon className="p-1 h-5 bg-yellow-400" />
       </div>
-      <div className="header_nav">
-        <div className="header_options">
-          <span class="header_optionLineOne">Hello Guests</span>
-          <span class="header_optionLineTwo">Sign In</span>
+      <div className="flex justify-evenly">
+        <div className="flex flex-col ml-3 text-white">
+          <span class="text-small">Hello Guests</span>
+          <span class="text-base font-medium">Sign In</span>
         </div>
-        <div className="header_options">
-          <span class="header_optionLineOne">Returns</span>
-          <span class="header_optionLineTwo">& Orders</span>
+        <div className="flex flex-col ml-3 text-white">
+          <span class="text-small">Returns</span>
+          <span class="text-base font-medium">& Orders</span>
         </div>
-        <div className="header_options">
-          <span class="header_optionLineOne">Your</span>
-          <span class="header_optionLineTwo">Prime</span>
+        <div className="flex flex-col ml-3 text-white">
+          <span class="text-small">Your</span>
+          <span class="text-base font-medium">Prime</span>
+        </div>
+        <div class="flex items-center text-white">
+          <ShoppingBasketIcon />
+          <span className="header_optionLineTwo header_basketCount">0</span>
         </div>
       </div>
     </div>
