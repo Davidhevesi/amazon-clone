bfa06fe7d7f29e973092cca877a6bf06a67c24c5
David Hevesi <davidhevesi2008@gmail.com>
2021-05-27 19:10:11 -0400

    Add route from logo to home.

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="h-16 flex items-center bg-gray-800 sticky z-50">
-      <img
-        className="w-24 object-contain mt-0 mb-5 m-5"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+      <Link to="/">
+        <img
+          className="w-24 object-contain mt-0 mb-5 m-5"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
       <div className="flex flex-1 items-center border rounde-lg">
         <input class="flex-1" type="text" />
         <SearchIcon className="p-1 h-5 bg-yellow-400" />
