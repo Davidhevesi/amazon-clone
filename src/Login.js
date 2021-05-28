b11c1ff865843ad6ca61858b28c830eb06e51da0
David Hevesi <davidhevesi2008@gmail.com>
2021-05-28 15:27:09 -0400

    Add styles for login component.

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="login">
-      <img
-        className="login__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-      />
+    <div className="flex flex-col items-center h-screen bg-white">
+      <Link to="/">
+        <img
+          className="w-32 object-contain mt-8 mb-5 m-5"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+      <div className="md:w-2/4 lg:w-1/4 object-fit flex flex-col border-4 border-solid border-gray-200 p-5">
+        <h1 class="font-medium mb-5 text-2xl">Sign-In</h1>
+        <form>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <div className="mt-1">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div className="mt-1">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-2xl placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+          <button className="bg-yellow-400 border-2 w-full h-8 border-solid  mt-2 border-yellow-100">
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button className="border-2 w-full h-8 border-solid mt-2 border-gray-200 bg-gray-100">
+          Create your amazon account
+        </button>
+      </div>
     </div>
   );
 };
