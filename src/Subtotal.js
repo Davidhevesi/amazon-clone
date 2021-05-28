8b2ffb081df9c4d86274b41c589ebd1c025f48c4
David Hevesi <davidhevesi2008@gmail.com>
2021-05-28 07:17:16 -0400

    Add items in the cart and show the subtotal.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,18 @@
 import React from "react";
 import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
+import { useStateValue } from "./StateProvider";
 
 const Subtotal = () => {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="flex flex-col justify-between w-64 h-40 bg-gray-100 border-2 border-solid borde rounded-md px-2 py-2 ">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              {/* Part of the homework */}
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="flex items-center">
               <input className="mr-2" type="checkbox" /> This order contains a
@@ -18,7 +21,7 @@ const Subtotal = () => {
           </>
         )}
         decimalScale={2}
-        value={0} // Part of the homework
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
