5fe4ab0fa709409434837796b358fb6b1ee997ed
David Hevesi <davidhevesi2008@gmail.com>
2021-05-28 16:10:41 -0400

    initialize database and auth.

--- src/firebase.js
+++ src/firebase.js
@@ -10,3 +10,9 @@ const firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
