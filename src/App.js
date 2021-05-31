77a78a301585e22418da740dbd46e048cc71d818
David Hevesi <davidhevesi2008@gmail.com>
2021-05-31 13:18:17 -0400

    Add more styles to payment component.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import React, { useEffect } from "react";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe("pk_test_DYDXWo0UukDGyXisg6XzBZ1p00H4z0ZkEL");
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -42,7 +46,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/checkout">
             <Header />
--- src/Payment.js
+++ src/Payment.js
@@ -1,34 +1,50 @@
-import React from "react";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
 
 const Payment = () => {
   const [{ basket, user }, dispatch] = useStateValue();
+  const stripe = useStripe();
+  const elements = useElements();
+  const [error, setError] =useState();
+  const [disabled, setDisabled] =useState();
 
+  const handleSubmit = e => {
+
+  }
+  const handleChange = e => {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  }
   return (
-    <div className="flex bg-white ">
-      <div className="payment_continer">
+    <div className="bg-white">
+      <div className="">
         <h1 className="text-center p-2 font-bold bg-white border-b-2 border-solid border-gray-200">
-          Checkout (<Link className="text-blue-200" to="/checkout">{basket?.length} items</Link>)
+          Checkout (
+          <Link className="text-purple-400" to="/checkout">
+            {basket?.length} items
+          </Link>
+          )
         </h1>
-        <div className="payment_section flex p-5">
-          <div className="payment_title">
-            <h3>Delivery address</h3>
+        <div className="flex p-5 border-b-2 border-gray-300 m-5 border-solid">
+          <div className="">
+            <h3 class="font-bold text-lg">Delivery address:</h3>
           </div>
-          <div className="payment_address">
+          <div className="ml-4">
             <p>{user?.email}</p>
             <p>321 Riverside</p>
             <p>Atlanta, Ga</p>
           </div>
         </div>
         {/* Payment section Review items */}
-        <div className="payment_section flex p-5">
-          <div className="payment_title">
-            <h3>Review items in the order</h3>
+        <div className="flex p-5 border-b-2 border-gray-300 m-5 border-solid">
+          <div className="">
+            <h3 class="font-bold text-lg">Review items in the order:</h3>
           </div>
 
-          <div className="payment_items">
+          <div className="">
             {basket.map((item) => (
               <CheckoutProduct
                 id={item.id}
@@ -41,11 +57,15 @@ const Payment = () => {
           </div>
         </div>
         {/* Payment section- Payment method */}
-        <div className="payment_section flex p-5">
-          <div className="payment_title">
-            <h3>Payment Method</h3>
+        <div className="">
+          <div className="flex-1">
+            <h3 class="font-bold text-lg">Payment Method:</h3>
           </div>
-          <div className="payment_detail"></div>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <CardElement onchange={handleChange} className="block w-full pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md" />
+            </form>
+          </div>
         </div>
       </div>
     </div>
